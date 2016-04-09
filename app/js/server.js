eda370e3cbf4bcaf936e3fec6cb0c17ad26f1d7d
Harish Krishnaswamy <harish@havenlife.com>
2016-04-09 16:03:37 -0400

    cleanup

--- app/js/server.js
+++ app/js/server.js
@@ -41,44 +41,6 @@ app.get('/api/pulse/refresh', function (req, res) {
         .catch(writeError.bind(null, res));
 });
 
-// app.get('/api/comments', function (req, res) {
-//     fs.readFile(COMMENTS_FILE, function (err, data) {
-//         if (err) {
-//             console.error(err);
-//             process.exit(1);
-//         }
-//         res.setHeader('Cache-Control', 'no-cache');
-//         res.json(JSON.parse(data.toString()));
-//     });
-// });
-
-// app.post('/api/comments', function (req, res) {
-//     fs.readFile(COMMENTS_FILE, function (err, data) {
-//         if (err) {
-//             console.error(err);
-//             process.exit(1);
-//         }
-//         var comments = JSON.parse(data.toString());
-//         // NOTE: In a real implementation, we would likely rely on a database or
-//         // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-//         // treat Date.now() as unique-enough for our purposes.
-//         var newComment = {
-//             id: Date.now(),
-//             author: req.body.author,
-//             text: req.body.text,
-//         };
-//         comments.push(newComment);
-//         fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function (err) {
-//             if (err) {
-//                 console.error(err);
-//                 process.exit(1);
-//             }
-//             res.setHeader('Cache-Control', 'no-cache');
-//             res.json(comments);
-//         });
-//     });
-// });
-
 app.listen(app.get('port'), function () {
     console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
